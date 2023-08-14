bf725a3ffae20cdb21ab9d80b7d2427c0b81f9e5
fabri <fabri@host.docker.internal>
2023-08-14 17:01:03 +0200

    1

---
+++ helloservice/src/test/java/com/fabrix/hello/HelloserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrix.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
