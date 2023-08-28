2cd0aeb83ff54882ba130d89b9c504502bfa4c65
fabri <fabri@LAPTOP-P1LMCRQ8.mshome.net>
2023-08-28 19:04:48 +0200

    docker compose files

--- hotels/src/main/java/com/fabrix/hotels/HotelsApplication.java
+++ hotels/src/main/java/com/fabrix/hotels/HotelsApplication.java
@@ -3,11 +3,13 @@ package com.fabrix.hotels;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.fabrix.hotels.controller") })
 @EnableJpaRepositories("com.fabrix.hotels.repository")
 @EntityScan("com.fabrix.hotels.model")
